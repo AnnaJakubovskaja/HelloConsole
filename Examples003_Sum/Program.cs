ae7896a0e7303cf5dd299616d84cfdb6c9dc6542
Anna Jakubovskaja <anna28.78@bk.ru>
2022-11-15 17:32:15 +0300

    Examples003_Sum prodjekt

--- Examples003_Sum/Program.cs
+++ Examples003_Sum/Program.cs
@@ -1,5 +1,5 @@
-﻿int numberA = new Random().Next(1,10); // 1,2,3...10
+﻿int numberA = new Random().Next(1, 10); // 1,2,3...10
 Console.WriteLine = numberA;
-int numberB = new Random().Next(1,10);
+int numberB = new Random().Next(1, 10);
 Console.WriteLine = numberA;
 Console.WriteLine(numberA + numberB);
